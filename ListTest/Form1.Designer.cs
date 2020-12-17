b76c87abce6bc227d348c52ddca0afb1024bad45
Tony Theodoropoulos <theodot@outlook.com>
2020-12-17 13:04:17 -0500

    Updated the design

--- ListTest/Form1.Designer.cs
+++ ListTest/Form1.Designer.cs
@@ -29,81 +29,125 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.displayButton = new System.Windows.Forms.Button();
-            this.originalOutput = new System.Windows.Forms.Label();
-            this.sortOutput = new System.Windows.Forms.Label();
-            this.reverseOutput = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.inputBox = new System.Windows.Forms.TextBox();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.removeButton = new System.Windows.Forms.Button();
             this.addButton = new System.Windows.Forms.Button();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.reverseOutput = new System.Windows.Forms.Label();
+            this.sortOutput = new System.Windows.Forms.Label();
+            this.originalOutput = new System.Windows.Forms.Label();
+            this.displayButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // displayButton
+            // titleLabel
             // 
-            this.displayButton.Location = new System.Drawing.Point(33, 80);
-            this.displayButton.Name = "displayButton";
-            this.displayButton.Size = new System.Drawing.Size(339, 32);
-            this.displayButton.TabIndex = 0;
-            this.displayButton.Text = "Display";
-            this.displayButton.UseVisualStyleBackColor = true;
-            this.displayButton.Click += new System.EventHandler(this.displayButton_Click);
+            this.titleLabel.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(-3, 0);
+            this.titleLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(415, 52);
+            this.titleLabel.TabIndex = 34;
+            this.titleLabel.Text = "List Examples";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // originalOutput
+            // removeButton
             // 
-            this.originalOutput.BackColor = System.Drawing.Color.White;
-            this.originalOutput.Location = new System.Drawing.Point(30, 124);
-            this.originalOutput.Name = "originalOutput";
-            this.originalOutput.Size = new System.Drawing.Size(91, 175);
-            this.originalOutput.TabIndex = 1;
+            this.removeButton.BackColor = System.Drawing.Color.Black;
+            this.removeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.removeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.removeButton.ForeColor = System.Drawing.Color.White;
+            this.removeButton.Location = new System.Drawing.Point(288, 119);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(87, 31);
+            this.removeButton.TabIndex = 33;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = false;
             // 
-            // sortOutput
+            // addButton
             // 
-            this.sortOutput.BackColor = System.Drawing.Color.White;
-            this.sortOutput.Location = new System.Drawing.Point(153, 124);
-            this.sortOutput.Name = "sortOutput";
-            this.sortOutput.Size = new System.Drawing.Size(91, 175);
-            this.sortOutput.TabIndex = 2;
+            this.addButton.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.addButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addButton.ForeColor = System.Drawing.Color.White;
+            this.addButton.Location = new System.Drawing.Point(190, 119);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(87, 31);
+            this.addButton.TabIndex = 32;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = false;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
             // 
-            // reverseOutput
+            // inputBox
             // 
-            this.reverseOutput.BackColor = System.Drawing.Color.White;
-            this.reverseOutput.Location = new System.Drawing.Point(281, 124);
-            this.reverseOutput.Name = "reverseOutput";
-            this.reverseOutput.Size = new System.Drawing.Size(91, 175);
-            this.reverseOutput.TabIndex = 3;
+            this.inputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputBox.Location = new System.Drawing.Point(150, 80);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(225, 22);
+            this.inputBox.TabIndex = 31;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(30, 35);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(33, 83);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 13);
-            this.label1.TabIndex = 4;
+            this.label1.Size = new System.Drawing.Size(93, 16);
+            this.label1.TabIndex = 30;
             this.label1.Text = "Enter a Name:";
             // 
-            // inputBox
+            // reverseOutput
             // 
-            this.inputBox.Location = new System.Drawing.Point(115, 29);
-            this.inputBox.Name = "inputBox";
-            this.inputBox.Size = new System.Drawing.Size(191, 20);
-            this.inputBox.TabIndex = 5;
+            this.reverseOutput.BackColor = System.Drawing.Color.White;
+            this.reverseOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reverseOutput.Location = new System.Drawing.Point(272, 216);
+            this.reverseOutput.Name = "reverseOutput";
+            this.reverseOutput.Size = new System.Drawing.Size(103, 146);
+            this.reverseOutput.TabIndex = 29;
             // 
-            // addButton
+            // sortOutput
             // 
-            this.addButton.Location = new System.Drawing.Point(312, 23);
-            this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(58, 31);
-            this.addButton.TabIndex = 6;
-            this.addButton.Text = "Add";
-            this.addButton.UseVisualStyleBackColor = true;
-            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            this.sortOutput.BackColor = System.Drawing.Color.White;
+            this.sortOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sortOutput.Location = new System.Drawing.Point(153, 216);
+            this.sortOutput.Name = "sortOutput";
+            this.sortOutput.Size = new System.Drawing.Size(103, 146);
+            this.sortOutput.TabIndex = 28;
+            // 
+            // originalOutput
+            // 
+            this.originalOutput.BackColor = System.Drawing.Color.White;
+            this.originalOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.originalOutput.Location = new System.Drawing.Point(33, 216);
+            this.originalOutput.Name = "originalOutput";
+            this.originalOutput.Size = new System.Drawing.Size(103, 146);
+            this.originalOutput.TabIndex = 27;
+            // 
+            // displayButton
+            // 
+            this.displayButton.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.displayButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.displayButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayButton.ForeColor = System.Drawing.Color.White;
+            this.displayButton.Location = new System.Drawing.Point(32, 170);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(343, 32);
+            this.displayButton.TabIndex = 26;
+            this.displayButton.Text = "Display";
+            this.displayButton.UseVisualStyleBackColor = false;
+            this.displayButton.Click += new System.EventHandler(this.displayButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DarkKhaki;
-            this.ClientSize = new System.Drawing.Size(411, 323);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(411, 386);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.removeButton);
             this.Controls.Add(this.addButton);
             this.Controls.Add(this.inputBox);
             this.Controls.Add(this.label1);
@@ -113,7 +157,7 @@
             this.Controls.Add(this.displayButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "List Examples";
+            this.Text = "Lists";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,13 +165,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Button displayButton;
-        private System.Windows.Forms.Label originalOutput;
-        private System.Windows.Forms.Label sortOutput;
-        private System.Windows.Forms.Label reverseOutput;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button removeButton;
         private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label reverseOutput;
+        private System.Windows.Forms.Label sortOutput;
+        private System.Windows.Forms.Label originalOutput;
+        private System.Windows.Forms.Button displayButton;
     }
 }
 
